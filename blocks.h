cc7c47614533a8863b62bc1a33748c0a21cb3de1
Stephen Turley <stephen@turley.dev>
2020-05-12 21:45:17 -0400

    added volume

--- blocks.h
+++ blocks.h
@@ -2,11 +2,12 @@
 static const Block blocks[] = {
     /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-    {"☀", "xbacklight | sed 's/\\..*//'",		    		0,		10},
+    {"☀", "xbacklight | sed 's/\\..*//'",		    		0,		4},
     {"", "battery",                     					5,		3},
+    {"🔊", "volume",                   				    	0,		2},
     {" ", "date +\"%A %d %b %Y %H:%M\"",					60,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 //// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
-static char delim = '|';
+static char delim = ' ';
